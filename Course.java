de7a5c4472d3bfc8b73b86ceba234bdfafa680ca
Adam Grunwald <adamgrunwald13791@gmail.com>
2022-03-27 16:15:55 -0500

    Course Id

--- Course.java
+++ Course.java
@@ -5,7 +5,6 @@ public class Course {
 	private int id;
 	private String name;
 	private ArrayList<Course> prereqs;
-	private ArrayList<Session> sessions;
 	
 	public Course(String name) {
 		this.name = name;
@@ -13,18 +12,14 @@ public class Course {
 	public Course(String name, ArrayList<Course> prereqs) {
 		this.name = name;
 		this.prereqs = prereqs;
-		this.sessions = new ArrayList<>();
 	}
-	public Course(String name, ArrayList<Course> prereqs, ArrayList<Session> sessions) {
+	public Course(String name, ArrayList<Course> prereqs, int id) {
 		this.name = name;
 		this.prereqs = prereqs;
-		this.sessions = sessions;
+		this.id = id;
 	}
 	
 	
-	public void addSession(String prof, String days, String time) {
-		sessions.add(new Session(prof, days, time));
-	}
 	public void addPrereq(Course c) {
 		prereqs.add(c);
 	}
@@ -37,6 +32,9 @@ public class Course {
 	}
 	public int getId() {
 		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 }
