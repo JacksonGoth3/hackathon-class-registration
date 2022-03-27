db3e0171b7ff1427b771fcd17d0a7d31ba84293a
Adam Grunwald <adamgrunwald13791@gmail.com>
2022-03-27 14:39:50 -0500

    Added Session class

--- src/Course.java
+++ src/Course.java
@@ -4,7 +4,7 @@ public class Course {
 	
 	private String name;
 	private ArrayList<Course> prereqs;
-	private ArrayList<String> profs;
+	private ArrayList<Session> sessions;
 	
 	public Course(String name) {
 		this.name = name;
@@ -12,16 +12,17 @@ public class Course {
 	public Course(String name, ArrayList<Course> prereqs) {
 		this.name = name;
 		this.prereqs = prereqs;
+		this.sessions = new ArrayList<>();
 	}
-	public Course(String name, ArrayList<Course> prereqs, ArrayList<String> profs) {
+	public Course(String name, ArrayList<Course> prereqs, ArrayList<Session> sessions) {
 		this.name = name;
 		this.prereqs = prereqs;
-		this.profs = profs;
+		this.sessions = sessions;
 	}
 	
 	
-	public void addProf(String name) {
-		profs.add(name);
+	public void addSession(String prof, String days, String time) {
+		sessions.add(new Session(prof, days, time));
 	}
 	public void addPrereq(Course c) {
 		prereqs.add(c);
