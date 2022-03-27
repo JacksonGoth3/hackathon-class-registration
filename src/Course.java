3ce96d9122487b38ad5599fec1fd940fe694efe1
Adam Grunwald <adamgrunwald13791@gmail.com>
2022-03-27 14:19:33 -0500

    Added more Course methods

--- src/Course.java
+++ src/Course.java
@@ -9,14 +9,22 @@ public class Course {
 	public Course(String name) {
 		this.name = name;
 	}
-	public Course(String name, ArrayList<String> profs) {
+	public Course(String name, ArrayList<Course> prereqs) {
 		this.name = name;
+		this.prereqs = prereqs;
+	}
+	public Course(String name, ArrayList<Course> prereqs, ArrayList<String> profs) {
+		this.name = name;
+		this.prereqs = prereqs;
 		this.profs = profs;
 	}
 	
 	
 	public void addProf(String name) {
 		profs.add(name);
+	}
+	public void addPrereq(Course c) {
+		prereqs.add(c);
 	}
 	public ArrayList<Course> getPrereqs() {
 		return prereqs;
